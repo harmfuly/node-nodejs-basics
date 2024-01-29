c71fe259b829144b46362ea6b8e6c74452c0bb6a
harmfuly <harmfuly7@gmail.com>
2024-01-29 16:10:24 +0200

    feat: implemented function in rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import { access, rename as fsRename } from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const sourceFile = 'files/wrongFilename.txt';
+    const newFile = 'files/properFilename.md';
+
+    try {
+        await access(sourceFile);
+
+        try {
+           await access(newFile);
+           throw new Error('FS operation failed: properFilename.md already exists');
+        }
+        catch (newFileError) {
+            if (newFileError.code === 'ENOENT') {
+                await fsRename(sourceFile, newFile);
+
+                console.log('File renamed successfully');
+            } else {
+                throw new Error(`FS operation failed: ${newFileError.message}`);
+        }
+    }
+    } catch (sourceFileError) {
+        if (sourceFileError.code === 'ENOENT') {
+            throw new Error ('FS operation failed: wrongFilename.txt is not found')
+        } else {
+            throw new Error(`FS operation failed: ${sourceFileError.message}`);
+        }
+    }
 };
 
-await rename();
+rename().catch((err) => console.error(err));
