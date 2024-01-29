f384190accaf33598fa819b836f5e4bf6d43d54f
harmfuly <harmfuly7@gmail.com>
2024-01-29 19:00:16 +0200

    feat: implemented function in read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { access, readFile } from 'fs/promises'
+
 const read = async () => {
-    // Write your code here 
+    const filePath = 'files/fileToRead.txt';
+
+    try {
+        await access(filePath);
+
+        const fileContent = await readFile(filePath, 'utf-8');
+        console.log('Content of file:', fileContent);
+    }
+    catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed: File fileToRead.txt is not found.');
+        } else {
+            throw new Error(`FS operation failed: ${error.message}`);
+        }
+    }
 };
 
-await read();
+read().catch((err) => console.error(err));
