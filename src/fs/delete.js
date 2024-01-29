b4e5a08e375ad40c25b95a13b1a6d79dfc8729d9
harmfuly <harmfuly7@gmail.com>
2024-01-29 16:44:36 +0200

    feat: implemented function in delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { error } from 'console';
+import { access, unlink } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemove = 'files/fileToRemove.txt';
+    
+    try {
+        await access(fileToRemove);
+        await unlink(fileToRemove);
+        console.log('File fileToRemove.txt removed successfully.');
+    } catch (fileToRemoveError) {
+        if (fileToRemoveError.code === 'ENOENT') {
+            throw new Error('FS operation failed. File fileToRemove.txt not found.')
+        } else {
+            throw new Error(`FS operation failed: ${error.message}`)
+        }
+    }
 };
 
-await remove();
+remove().catch((err) => console.error(err));
