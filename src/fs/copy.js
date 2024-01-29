04389f595e17daf702fa221d26943a5aabef72fe
harmfuly <harmfuly7@gmail.com>
2024-01-29 13:39:12 +0200

    feat: implemented function in copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,43 @@
+import { access, readdir, copyFile, mkdir } from 'fs/promises';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+    const sourceFolder = 'files';
+    const newFolder = 'files_copy';
+
+    try {
+        await access(sourceFolder);
+
+        try {
+            await access(newFolder);
+            throw new Error('FS operation failed: New folder already exists');
+        }
+        catch (newFolderError) {
+            if (newFolderError.code === 'ENOENT') {
+                await mkdir(newFolder);
+
+                const files = await readdir(sourceFolder);
+
+                for (const file of files) {
+                    const sourceFilePath = path.join(sourceFolder, file);
+                    const newFilePath = path.join(newFolder, file);
+
+                    await copyFile(sourceFilePath, newFilePath);
+                }
+
+                console.log('Folder copied successfully.');
+
+            } else {
+                throw new Error (`FS operation failed: ${newFolderError.message}`);
+                }
+            }
+        } catch (sourceFolderError) {
+            if (sourceFolderError.code === 'ENOENT') {
+                throw new Error('FS operation failed: Source folder not found');
+            } else {
+                throw new Error(`FS operation failed: ${sourceFolderError.message}`);
+            }
+        }
 };
 
-await copy();
+copy().catch((err) => console.error(err));
