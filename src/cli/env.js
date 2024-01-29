807257bb53e931e084b2ec0fdf965f3852ddd26d
harmfuly <harmfuly7@gmail.com>
2024-01-29 20:52:20 +0200

    feat: implemented function in env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVariables = process.env;
+
+    console.log('Environment variables with prefix RSS_:');
+
+    for (const variable in envVariables) {
+        if (variable.startsWith('RSS_')) {
+            console.log(`${variable}=${envVariables[variable]}`);
+        }
+    }
 };
+
+export { parseEnv };
 
 parseEnv();
