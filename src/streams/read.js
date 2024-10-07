9d946a4af99f505e17b6244d4c7d034778f5ac75
harmfuly <harmfuly7@gmail.com>
2024-10-07 13:48:20 +0300

    feat: implemented function in streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { createWriteStream } from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
-};
+    const readableFilePath = path.join(process.cwd(), 'src/streams/files/fileToRead.txt');
+    const wrirebleFilePath = path.join(process.cwd(), 'src/streams/files/fileToWrite.txt');
+    const readableStream = createReadStream(readableFilePath, 'utf-8');
+    const wrirebleStream = createWriteStream(wrirebleFilePath, 'utf-8');
+
+    readableStream.on('data', (chunk) => {
+        wrirebleStream.write(chunk)
+    });
+    readableStream.on('error', (err) => console.error(`Error reading file: ${err.message}`));
+
+}
 
 await read();
