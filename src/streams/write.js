9e576eb6f14c01619362ee3b082cdf96340ed88b
harmfuly <harmfuly7@gmail.com>
2024-10-07 14:52:02 +0300

    feat: implemented function in streams/transform.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -3,20 +3,13 @@ import path from 'path';
 
 const write = async () => {
     process.stdin.setEncoding('utf-8');
-    console.log('Enter text to write to the file (type "exit" to finish):');
+    console.log('Enter text to write to the file (press Ctrl+C to exit):');
     
     const writablePath = path.join(process.cwd(), 'src/streams/files/fileToWrite.txt');
     const writableStream = createWriteStream(writablePath, { flags: 'a', encoding: 'utf-8' });
 
     process.stdin.on('data', data => {
         const trimData = data.trim();
-        if (trimData.toLowerCase() === 'exit') {
-            writableStream.end();
-            console.log('Writing to the file is done.');
-            process.stdin.pause();
-            return;
-        }
-
         writableStream.write(data);
         console.log('You entered:', trimData);
     });
