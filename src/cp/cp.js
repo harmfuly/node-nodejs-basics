d5eec2bca4bd27e3958620ed350d5a8c3c4c3d80
harmfuly <harmfuly7@gmail.com>
2024-10-07 21:13:59 +0300

    feat: implemented functions in cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,28 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from 'node:child_process';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'path';
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const spawnChildProcess = async (args) => {
+    const currentFilePath = fileURLToPath(import.meta.url);
+    const currentDirPath = dirname(currentFilePath);
+
+    const child = spawn('node', [resolve(currentDirPath, 'files', 'script.js'), ...args], {
+        stdio: ['pipe', 'pipe', 'inherit'],
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+    child.on('error', (err) => {
+        console.error(`Failed to start child process: ${error.message}`);
+    });
+
+};
+console.log('press Ctrl+C to exit');
+
+spawnChildProcess( ['arg1', 'arg2'] );
