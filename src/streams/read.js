bd3628379efee74d65666b919f2da438747f175b
harmfuly <harmfuly7@gmail.com>
2024-10-07 14:03:07 +0300

    fix: changed output method on into process.stdout

--- src/streams/read.js
+++ src/streams/read.js
@@ -4,15 +4,10 @@ import path from 'path';
 
 const read = async () => {
     const readableFilePath = path.join(process.cwd(), 'src/streams/files/fileToRead.txt');
-    const wrirebleFilePath = path.join(process.cwd(), 'src/streams/files/fileToWrite.txt');
     const readableStream = createReadStream(readableFilePath, 'utf-8');
-    const wrirebleStream = createWriteStream(wrirebleFilePath, 'utf-8');
 
-    readableStream.on('data', (chunk) => {
-        wrirebleStream.write(chunk)
-    });
+    readableStream.pipe(process.stdout)
     readableStream.on('error', (err) => console.error(`Error reading file: ${err.message}`));
-
 }
 
 await read();
