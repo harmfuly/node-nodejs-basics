d371bdd9bcbd8189502cf3bf54464e9b1fbf2305
harmfuly <harmfuly7@gmail.com>
2024-10-07 15:32:54 +0300

    feat: implemented function in zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createWriteStream, createReadStream } from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+
 const compress = async () => {
-    // Write your code here 
+    const fileToCompress = createReadStream(path.join(process.cwd(), 'src/zip/files/fileToCompress.txt'));
+
+    const gzip = zlib.createGzip();
+
+    const compressedFile = createWriteStream(path.join(process.cwd(), 'src/zip/files/archive.gz'));
+    fileToCompress.pipe(gzip).pipe(compressedFile);
+
+    compressedFile.on('finish', () => {
+        console.log('File has been compressed successfully');
+    });
+
+    compressedFile.on('error', (err) => {
+        console.error(`Error writing compressed file: ${err.message}`);
+    });
+
+    fileToCompress.on('error', (err) => {
+        console.error(`Error reading file to compress: ${err.message}`);
+    });
 };
 
 await compress();
