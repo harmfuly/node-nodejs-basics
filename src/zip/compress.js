c1193ea9597561541a08cdc60ee874677d0bbef8
harmfuly <harmfuly7@gmail.com>
2024-10-07 19:23:42 +0300

    feat: implemented function in zip/decompress.js, added deletion in compess function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,17 +1,27 @@
 import { createWriteStream, createReadStream } from 'fs';
 import zlib from 'zlib';
 import path from 'path';
+import { unlink } from 'fs/promises';
 
 const compress = async () => {
-    const fileToCompress = createReadStream(path.join(process.cwd(), 'src/zip/files/fileToCompress.txt'));
+    const fileToCompressPath = path.join(process.cwd(), 'src/zip/files/fileToCompress.txt');
+    const compressedFilePath = path.join(process.cwd(), 'src/zip/files/archive.gz');
 
+    const fileToCompress = createReadStream(fileToCompressPath);
     const gzip = zlib.createGzip();
-
-    const compressedFile = createWriteStream(path.join(process.cwd(), 'src/zip/files/archive.gz'));
+    const compressedFile = createWriteStream(compressedFilePath);
+    
     fileToCompress.pipe(gzip).pipe(compressedFile);
 
     compressedFile.on('finish', () => {
         console.log('File has been compressed successfully');
+        unlink(fileToCompressPath, err => {
+            if (err) {
+                console.error(`Error deleting file: ${err.message}`);
+            } else {
+                console.log('File has been deleted successfully');
+            }
+        })
     });
 
     compressedFile.on('error', (err) => {
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,35 @@
+import { createWriteStream, createReadStream } from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import { unlink } from 'fs/promises';
+
 const decompress = async () => {
-    // Write your code here 
+   const fileForDecompressPath = path.join(process.cwd(), 'src/zip/files/archive.gz');
+   const decompressedFilePath = path.join(process.cwd(), 'src/zip/files/fileToCompress.txt');
+
+   const fileForDecompress = createReadStream(fileForDecompressPath);
+   const unzip = zlib.createGunzip();
+
+   const decompressedFile = createWriteStream(decompressedFilePath);
+   fileForDecompress.pipe(unzip).pipe(decompressedFile);
+   decompressedFile.on('finish', () => {
+    console.log('File has been decompressed successfully');
+    unlink(fileForDecompressPath , err => {
+        if (err) {
+            console.error(`Error deleting file: ${err.message}`);
+        } else {
+            console.log('File has been deleted successfully');
+        }
+    })
+   });
+
+   decompressedFile.on('error', (err) => {
+    console.error(`Error during decompression:', ${err.message}`);
+   });
+
+   fileForDecompress.on('error', (err) => {
+    console.error(`Error reading file to decompress: ${err.message}`);
+   });
 };
 
 await decompress();
