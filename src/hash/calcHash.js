eba1c0ab003c0533d47a457e12b7fefe5eba36bc
harmfuly <harmfuly7@gmail.com>
2024-10-07 13:23:55 +0300

    feat: implemented function in calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(process.cwd(), 'src/hash/files/fileToCalculateHashFor.txt');
+    const fileStream = createReadStream(filePath);
+    const hash = createHash('sha256');
+
+    fileStream.on('data', (chunk) => hash.update(chunk));
+    fileStream.on('end', () => {
+        const resulthash = hash.digest('hex');
+        console.log(`Hash: ${resulthash}`);
+    });
+    fileStream.on('error', (err) => console.error(`Error reading file: ${err.message}`));
 };
 
 await calculateHash();
